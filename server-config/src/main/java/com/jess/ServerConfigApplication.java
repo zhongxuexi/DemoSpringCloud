6d195bdb087e67231c47a9d5ed9091e1fbfd4171
jess.zhong <jess.zhong@aliyun.com>
2018-09-10 23:02:38 +0800

    SpringCloud配置中心搭建服务端

---
+++ server-config/src/main/java/com/jess/ServerConfigApplication.java
@@ -0,0 +1,15 @@
+package com.jess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConfigApplication.class, args);
+	}
+}
